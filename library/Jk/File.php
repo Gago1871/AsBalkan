30cea8a672e91533cfb3d6e10e2c59c47bbca5b6
Jakub Kułak <jakub.kulak@gmail.com>
2012-05-02 08:58:51 +0200

    fixes in file upload

--- library/Jk/File.php
+++ library/Jk/File.php
@@ -65,9 +65,11 @@ class Jk_File
      */
     public function getMimeType($filename)
     {
-        $finfo = finfo_open(FILEINFO_MIME);
-        $data = explode('; ', finfo_file($finfo, $filename)); 
-        return $data[0];
+        // $finfo = finfo_open(FILEINFO_MIME);
+        // $data = explode('; ', finfo_file($finfo, $filename)); 
+        // return $data[0];
+
+        return mime_content_type($filename);
     }
 
 
